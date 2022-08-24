5d35cdc92047341093be677c8473120483bed77a
Fayyoz24 <fayyozusmon@gmail.com>
2022-08-24 20:55:57 +0200

    debug=flase

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,8 @@ env.read_env()
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+# DEBUG = env.bool('DEBUG', default=False)
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
